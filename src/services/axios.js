89e3a90b761211fb32e4209c6a784cc79c0ce9e9
dan.vd <dan.vd@amanotes.com>
2022-03-04 23:55:22 +0700

    update env & api url

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: `${process.env.REACT_APP_API_PROTOCAL}://${process.env.REACT_APP_API_HOST}:${process.env.REACT_APP_API_PORT}/api`,
+    baseURL: `${process.env.REACT_APP_API_BASE_URL}/api`,
     headers: {
         "Content-type": "application/json"
     }
